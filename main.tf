02c0ae1555ec09fb38c8c72e54adf1752c1a5a06
Maksat <maksat.kurk@gmail.com>
2022-01-26 00:49:19 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "s3" {
+    region = "us-west-2"
+    encrypt = true
+    bucket = "maxat-test"
+    key = "terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
---
+++ service.tf
@@ -0,0 +1,25 @@
+resource "aws_ecs_service" "aws-ecs-service" {
+  name                 = "${var.app_name}-${var.app_environment}-ecs-service"
+  cluster              = aws_ecs_cluster.aws-ecs-cluster.id
+  launch_type          = "FARGATE"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = 1
+  force_new_deployment = true
+
+  network_configuration {
+    subnets          = aws_subnet.private.*.id
+    assign_public_ip = false
+    security_groups = [
+      aws_security_group.service_security_group.id,
+      aws_security_group.load_balancer_security_group.id
+    ]
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "${var.app_name}-${var.app_environment}-container"
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
